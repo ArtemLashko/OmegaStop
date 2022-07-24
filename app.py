e685223a3bd5ac5f06bd4887815b6edf9b772144
Artem Lashko <48072376+ArtemLashko@users.noreply.github.com>
2022-07-24 13:54:18 -0500

    Revert "Revert "This is the initial state of this project.""
    This reverts commit 7dbd3027cf7ec9d71ed79a1c2648ca1c343f8df7.

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask import request, jsonify
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+# A route to return a next move
+@app.route('/api/v1/suggest_move', methods=['GET'])
+def api_all():
+    print(request.args)
+    game="notfound"
+    if 'game' in request.args:
+        game = str(request.args['game'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    return str(len(game))
+
+
+if __name__ == '__main__':
+    app.run()
