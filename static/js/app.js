c908c1b3658795bdbcec43e7365a5c8c3e83c09e
Artem Lashko <48072376+ArtemLashko@users.noreply.github.com>
2022-07-26 17:56:21 -0500

    refactored javascript code

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,39 @@
 var board = null
 var game = new Chess()
-
+var startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"  // This is a starting position
+var side = 'b'
 setTimeout(function() {
   board = Chessboard('board2', config)
-  $('#startBtn').on('click', game.start)
-  $('#clearBtn').on('click', game.clear)
+  $('#startBtn').on('click', startgame)
+  $('#clearBtn').on('click', board.clear)
+  $('#swapBtn').on('click', flip)
 }, 0);
+
+// This function starts the board
+function startgame(){
+  board.position(startFen)
+  game.reset()
+  board.start()
+}
+
+// Flips the board and makes a move
+function flip(){
+  board.flip()
+  side = 'w'
+  window.setTimeout(makeMove, 250)
+}
+
 
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
 
   // only pick up pieces for White
-  if (piece.search(/^b/) !== -1) return false
+  if (piece.search(/^${side}/) !== -1) return false
 }
 
-function makeRandomMove () {
+// This Function calls our server's api for a move
+function makeMove () {
   var fen = game.fen()
   $.get("http://127.0.0.1:5000/api/v1/suggest_move?fen=" + fen, function(data) {
         console.log("lol")
@@ -28,6 +46,9 @@ function makeRandomMove () {
   // game over
 }
 
+// This function is responsible for the process of making a move
+// If the move is not legal, it won't allow it
+// If it's legal it call function makeMove
 function onDrop (source, target) {
   // see if the move is legal
   var move = game.move({
@@ -39,8 +60,8 @@ function onDrop (source, target) {
   // illegal move
   if (move === null) return 'snapback'
 
-  // make random legal move for black
-  window.setTimeout(makeRandomMove, 250)
+  // make move
+  window.setTimeout(makeMove, 250)
 }
 
 // update the board position after the piece snap
