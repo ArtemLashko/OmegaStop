e3413063727fc1c3046506a70bf568c073d42026
Artem Lashko <48072376+ArtemLashko@users.noreply.github.com>
2022-08-02 17:00:55 -0500

    Fixed bug with a position evaluation

--- chessengine.py
+++ chessengine.py
@@ -9,11 +9,12 @@ import tensorflow
 class Engine:
     INF = 99999999999
     NEG_INF = -99999999999
-    ML_N = 2  # this constant is used to tune the usage of machine learning
+    ML_N = 1  # this constant is used to tune the usage of machine learning
 
     def __init__(self, fen):
         self.board = chess.Board()
-        self.model = load_model("fBIGelo1")
+        self.model = load_model("fBIGelo1")  # Machine learning model
+
         ##########################
         # Values of pieces
         ##########################
@@ -90,14 +91,17 @@ class Engine:
                                   }
         self.board.set_fen(fen)  # Initialize the board with a fen string
 
+    # The purpose of this function is to give some board configuration an evaluation
+    #  in terms of pawns and position
     def position_eval(self):
         score = 0
         # Chess library interpreter "WHITE" color as True, and "BLACK" as False
         for piece_type in self.pieces.keys():
             score += len(self.board.pieces(self.pieces_to_number[piece_type], True)) * self.pieces[piece_type]
             score -= len(self.board.pieces(self.pieces_to_number[piece_type], False)) * self.pieces[piece_type]
-            for ind in self.board.pieces(self.pieces_to_number[piece_type], True):
-                score -= self.evaluation_tables[piece_type][-ind]
+            # Utilize Piece-Square tables for calculating a position advantage
+            for ind in self.board.pieces(self.pieces_to_number[piece_type], False):
+                score += self.evaluation_tables[piece_type][-ind]
             for ind in self.board.pieces(self.pieces_to_number[piece_type], True):
                 score += self.evaluation_tables[piece_type][ind]
         return score
@@ -110,23 +114,29 @@ class Engine:
     # Returns the best tactical chess move for any position
     # The recommended depth is 4 - 5 since minimax without optimizations
     def minimax(self, depth, mx_depth):
-        maxormin = self.board.turn
-        legal_moves = list(self.board.legal_moves)
-        bestmove = ""
-        if depth == 0 or self.board.is_game_over():
+        whiteturn = self.board.turn  # is it a white move 1 - if it's 0 if it's not
+        legal_moves = list(self.board.legal_moves)  # list of legal moves for a given position
+        bestmove = "" if len(legal_moves) == 0 else str(legal_moves[0])  # this is the best move in string format
+
+        if depth == 0 or self.board.is_game_over():  # the game is over,or we don't want to look anymore
             return self.position_eval()
-        if maxormin:
+
+        # If it is a white turn, we want to maximize a score. If it is black, we want to minimize
+        if whiteturn:
             max_score = self.NEG_INF
         else:
             max_score = self.INF
+
+        # Let's iterate over potential moves and investigate opportunities
         for move in legal_moves:
             self.board.push(move)
             temp_score = self.minimax(depth - 1, mx_depth)
             self.board.pop()
-            if (maxormin == 1) and (temp_score > max_score):
+
+            if (whiteturn == 1) and (temp_score > max_score):
                 bestmove = str(move)
                 max_score = temp_score
-            if (maxormin == 0) and (temp_score < max_score):
+            if (whiteturn == 0) and (temp_score < max_score):
                 bestmove = str(move)
                 max_score = temp_score
             if depth == mx_depth:
@@ -138,12 +148,15 @@ class Engine:
         return max_score
 
     def alphabeta(self, depth, mx_depth, alpha, beta, last_move=""):
-        maxormin = self.board.turn
-        legal_moves = list(self.board.legal_moves)
-        bestmove = "" if len(legal_moves) == 0 else str(legal_moves[0])
-        if depth == 0 or self.board.is_game_over():
+        whiteturn = int(self.board.turn)  # is it a white move 1 - if it's 0 if it's not
+        legal_moves = list(self.board.legal_moves)  # list of legal moves for a given position
+        bestmove = "" if len(legal_moves) == 0 else str(legal_moves[0])  # this is the best move in string format
+
+        if depth == 0 or self.board.is_game_over():  # the game is over,or we don't want to look anymore
             return self.position_eval()
-        if maxormin:
+
+        # If it is a white turn, we want to maximize a score. If it is black we want to minimize
+        if whiteturn:
             max_score = self.NEG_INF
         else:
             max_score = self.INF
@@ -153,9 +166,9 @@ class Engine:
             lst = []
             for move in legal_moves:
                 self.board.push(move)
-                lst.append([self.ml_elal(), move])
+                lst.append([self.ml_eval(), move])
                 self.board.pop()
-            if maxormin:  # that's a white move, and we are interested in maximizing evaluation
+            if whiteturn:  # that's a white move, and we are interested in maximizing evaluation
                 lst.sort(reverse=True, key = lambda x: x[0])
             else:  # that's a black move, and we are interested in minimizing evaluation
                 lst.sort(key = lambda x: x[0])
@@ -163,25 +176,25 @@ class Engine:
             legal_moves = []
             for i in range(len(lst)):
                 legal_moves.append(lst[i][1])
-        #print(len(legal_moves))
 
-
+        # Let's iterate over potential moves and investigate opportunities
         for move in legal_moves:
             self.board.push(move)
             temp_score = self.alphabeta(depth - 1, mx_depth, alpha, beta, str(move))
             self.board.pop()
-            if (maxormin == 1) and (temp_score > max_score):
+
+            if (whiteturn == 1) and (temp_score > max_score):
                 bestmove = str(move)
                 max_score = temp_score
-            if (maxormin == 0) and (temp_score < max_score):
+            if (whiteturn == 0) and (temp_score < max_score):
                 bestmove = str(move)
                 max_score = temp_score
-            if maxormin == 1:
-                alpha = max(alpha, temp_score)
+            if whiteturn == 1:
+                alpha = max(alpha, max_score)
             else:
-                beta = min(beta, temp_score)
+                beta = min(beta, max_score)
             if beta <= alpha:
-                break
+                return max_score
             if depth == mx_depth:
                 print("Potential answer is " + str(move) + " : " + str(temp_score))
         if depth == mx_depth:
@@ -197,40 +210,47 @@ class Engine:
     def minimax_move(self):
         return self.minimax(4, 4)
 
-    # This function transforms a chess position to an input for a Machine Learning model
+    # This function transforms a chess position into an input for a Machine Learning model
+    # The model has 772 parameters for each color and figures what squares are not free
+    # 772 = 2 colors * 6 figures * 8 columns * 8 rows + can white castle + can black castle + who can move
     def make_a_row(self):
         lst = [0 for i in range(2 * 6 * 8 * 8)]
-        half = 384
+        half = 772 / 2
         for i in range(1, 7):
-            for x in self.board.pieces(i, False):
+            for x in self.board.pieces(i, False):  # black color
                 lst[0 + (i - 1) * 64 + x] = 1
-            for x in self.board.pieces(i, True):
+            for x in self.board.pieces(i, True):  # white color
                 lst[half + (i - 1) * 64 + x] = 1
         lst.append(int(self.board.has_castling_rights(True)))
         lst.append(int(self.board.has_castling_rights(False)))
         lst.append(int(self.board.turn))
         return lst
 
-    def ml_elal(self):
+    # This function uses a machine learning model to answer who has an advantage in any given board.
+    # 0 - if black has an advantage >= 1.5 pawns, 1 - if it's a draw (-1.5, 1.5), 2 - if white has >= 1.5 pawns
+    def ml_eval(self):
         input_list = self.make_a_row()
         output = self.model(np.array([input_list, ]))
         # print(output.numpy().tolist()[0])
         valid = [float(x) for x in output.numpy().tolist()[0]]
-        mx = 0
-        mx_ind = 0
+        mx = 0  # maximum probability
+        mx_ind = 0  # answer
 
         for i in range(len(valid)):
-            if (valid[i] > mx):
+            if valid[i] > mx:
                 mx = valid[i]
                 mx_ind = i
         return mx_ind
 
+    # Runs an alphabeta algorithm
     def alphabeta_move(self):
         return self.alphabeta(4,4, self.NEG_INF, self.INF)
 
+    # It returns white if it's a white move. Black otherwise.
     def whose_move(self):
         return "White" if self.board.turn else "Black"
 
+    # Prints debug information.
     def debug(self):
         print("##########################")
         print("FEN = " + str(self.board.fen()))
